9401d8cdef5f771b071c322e13f03f283e47befc
Nadyne14 <nadynerosalia1234@gmail.com>
2023-10-04 11:39:03 +0700

    commit 2

--- Praktikum05/PemilihanBilangan23.java
+++ Praktikum05/PemilihanBilangan23.java
@@ -1,15 +1,9 @@
 import java.util.Scanner;
 public class PemilihanBilangan23 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Masukkan sebuah angka; ");
-        int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else{
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil ");
-        }
+        Double angka = 4.4;
+        String hasil;
+        hasil = (angka < 2) ? "Bilangan ganjil" : "Bilangan genap";
+        System.out.println(angka + " adalah " + hasil);
     }
 }
