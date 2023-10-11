0d9c6b813c128ab37a27f75f88167b0905d00eb9
Nadyne14 <nadynerosalia1234@gmail.com>
2023-10-11 11:38:20 +0700

    commit 2

--- Praktikum05/Praktikum06/KabisatNested23.java
+++ Praktikum05/Praktikum06/KabisatNested23.java
@@ -7,10 +7,14 @@ public class KabisatNested23 {
         int tahun = sc.nextInt();
 
         if ((tahun % 4) == 0 ) {
-            if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
-        } else
-          System.out.println("Bukan Tahun Kabisat");
+            if ((tahun % 100 != 0)) 
+                System.out.println("Tahun Kabisat");
+            else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        } 
     }
 
 }
