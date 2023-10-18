bd98e7fd8aa37c20e51319202043cf23184d22d3
Nadyne14 <nadynerosalia1234@gmail.com>
2023-10-18 11:52:06 +0700

    commit 2

--- Praktikum07/ForKelipatan23.java
+++ Praktikum07/ForKelipatan23.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 public class ForKelipatan23 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan bilangan 1-9: ");
+        int bilangan = scanner.nextInt();
 
-        int bilangan;
         int total = 0;
         int counter = 0;
 
         total = 0;
         counter = 0;
 
-        System.out.println("Masukkan bilangan 1-9: ");
-        bilangan = scanner.nextInt();
 
         for (int i = 0; i < 50; i++) {
             if (i % bilangan == 0) {
@@ -19,6 +18,7 @@ public class ForKelipatan23 {
                 counter++;
             }
         }
+        double average = (double) total / counter;
         System.out.printf("Pada rentang 1-50 terdapat%d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
 
     }
