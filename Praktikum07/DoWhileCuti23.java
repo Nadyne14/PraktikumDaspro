711cc35f309ca19148598fafdd7c19e3a84c3474
Nadyne14 <nadynerosalia1234@gmail.com>
2023-10-18 14:21:26 +0700

    commit 1

--- Praktikum07/DoWhileCuti23.java
+++ Praktikum07/DoWhileCuti23.java
@@ -1,4 +1,32 @@
 import java.util.Scanner;
 public class DoWhileCuti23 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        int jatahCuti = 5;
+        int jumlahHari;
+        String konfirmasi;
+
+        System.out.println("Jatah Cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.println("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.nextLine();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+                System.out.println("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+
 }
