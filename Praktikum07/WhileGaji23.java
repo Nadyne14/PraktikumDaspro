5c54bed8591f7642b5061a5072d8db43352f885a
Nadyne14 <nadynerosalia1234@gmail.com>
2023-10-18 13:49:06 +0700

    commit 2

--- Praktikum07/WhileGaji23.java
+++ Praktikum07/WhileGaji23.java
@@ -29,6 +29,10 @@ public class WhileGaji23 {
                 gajiLembur = jumlahJamLembur * 100000;
             } else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahJamLembur * 75000;
+            } else {
+                System.out.println("Jabatan invalid");
+                i--;
+                continue;
             }
 
             totalGajiLembur += gajiLembur;
