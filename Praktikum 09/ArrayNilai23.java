c94fb0e93d197661a42a8f64a815ae62e9f37138
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-01 13:46:55 +0700

    commit 4

--- Praktikum 09/ArrayNilai23.java
+++ Praktikum 09/ArrayNilai23.java
@@ -4,21 +4,45 @@ public class ArrayNilai23 {
         
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-
+        int totalNilai = 0;
+        int nilaiTertinggi = Integer.MIN_VALUE;
+        int nilaiTerendah = Integer.MAX_VALUE;
+        int mahasiswaLulus = 0;
+        int mahasiswaTidakLulus = 0;
 
         for (int i =0; i < nilaiAkhir.length; i++) {
             System.out.println("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
+            totalNilai += nilaiAkhir[i];
+            if (nilaiAkhir[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiAkhir[i];
+            }
+            if (nilaiAkhir[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiAkhir[i];
+            }
+            if (nilaiAkhir[i] > 70){
+                mahasiswaLulus++;
+            } else {
+                mahasiswaTidakLulus++;
+            }
         }
 
         System.out.println("Status kelulusan mahasiswa: ");
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Nilai Akhir Mahasiswa Ke - " + (i+1) + " lulus ");
             } else {
                 System.out.println("Nilai akhir Mahasiswa Ke - " + (i + 1) + " tidak lulus");
             }           
         }
+
+        double rataRata = (double) totalNilai / nilaiAkhir.length;
+
+        System.out.println("Nilai Rata-rata: " + rataRata);
+        System.out.println("Nilai Tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai Terendah: " + nilaiTerendah);
+        System.out.println("Jumlah mahasiswa yang Lulus: " + mahasiswaLulus);
+        System.out.println("Jumlah mahasiswa yang Tidak Lulus: " + mahasiswaTidakLulus);
     }
 }
