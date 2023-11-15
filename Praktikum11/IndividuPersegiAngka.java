fcd3ced04684a814ce5b2620ba5e821725fcc2e9
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-15 15:26:26 +0700

    commit 1

---
+++ Praktikum11/IndividuPersegiAngka.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IndividuPersegiAngka {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = scanner.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == 1 || i == n || j == 1 || j == n) {
+                        System.out.print(" 3");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+        }
+    }
+}
