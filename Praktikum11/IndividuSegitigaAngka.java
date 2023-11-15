67317c5f8cbefda7858998c99b94b4761af7225d
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-15 15:12:21 +0700

    commit 1

---
+++ Praktikum11/IndividuSegitigaAngka.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IndividuSegitigaAngka {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = scanner.nextInt();
+
+       
+            for (int i = 1; i <= n; i++) {
+                for (int j = n; j >= i; j--) {
+                   System.out.print(" ");
+                }
+                for (int j  = 1; j <= i; j++) {
+                System.out.print(j);
+                }
+                System.out.println();
+        }
+    }
+}
