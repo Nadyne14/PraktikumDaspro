375cfa789c6b8a145f9bccac31cb2abb416570b4
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-16 14:06:25 +0700

    commit 2

--- Praktikum11/NestedLoop23.java
+++ Praktikum11/NestedLoop23.java
@@ -3,25 +3,39 @@ public class NestedLoop23 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner (System.in);
-        double[][] suhu = new double[2][4];
+
+        System.out.print("Masukkan jumlah kota: ");
+        int n = sc.nextInt();
+
+        double[][] suhu = new double[n][4];
+        double[] rataRata = new double[5];
 
         for (int i = 0; i < suhu.length; i++) {
-            System.out.println("KOTA KE-" + (i+1));
- 
+            System.out.println("KOTA KE-" + (i + 1));
+                double temp =0;
+
             for (int j = 0; j < suhu[i].length; j++) {
                 System.out.print("  Suhu ke-" + (j+1) + ": ");
                 suhu[i][j] = sc.nextInt();
+                temp += suhu [i][j];
             }
+            rataRata[i] = temp / suhu[i].length;
+            System.out.println();
         }
         
         for (int i = 0; i < suhu.length; i++) {
-            System.out.println("KOTA KE-" + (i+1) + ": ");
+            System.out.print("KOTA KE-" + (i+1) + ": ");
 
             for (int j = 0; j < suhu[i].length; j++) {
                 System.out.print(suhu[i][j] + " ");
             }
 
-            System.out.println();
+            //for (double temp : suhu[i]) {
+            //    System.out.println(temp);
+          //  }
+                
+            //System.out.println();
+            System.out.println("Rata rata suhu: " + rataRata[i]);
         }
     }
 }
