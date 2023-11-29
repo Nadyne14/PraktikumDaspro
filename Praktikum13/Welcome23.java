125ac2c3f65b78122fe6ea0a51672b10d57bbe86
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-29 10:48:45 +0700

    welcome1

---
+++ Praktikum13/Welcome23.java
@@ -0,0 +1,9 @@
+public class Welcome23 {
+    public static void displayWelcomeMessage() {
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+}
