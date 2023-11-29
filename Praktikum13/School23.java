6cefd1d21911430089b9179d876bfcf313be0206
Nadyne14 <nadynerosalia1234@gmail.com>
2023-11-29 13:48:57 +0700

    School 2

--- Praktikum13/School23.java
+++ Praktikum13/School23.java
@@ -5,42 +5,58 @@ public class School23 {
         String[] student1C = {"clara", "Celine", "charlie", "Caroine" };
         String[] student1D = {"Daniel", "daisy", "Diana" };
 
-        System.out.printf("=========================%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("=========================%n");
+        display(student1A);
+        display(student1B);
+        display(student1C);
+        display(student1D);
 
-        for (int i = 0; i < student1A.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i+1), student1A[i]);
-        }
-        System.out.printf("=========================%n");
+         
+        // System.out.printf("=========================%n");
+        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        // System.out.printf("=========================%n");
+
+        // for (int i = 0; i < student1A.length; i++) {
+        //     System.out.printf("| %-3d | %-15s |%n", (i+1), student1A[i]);
+        // }
+        // System.out.printf("=========================%n");
 
 
-        System.out.printf("=========================%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("=========================%n");
+        // System.out.printf("=========================%n");
+        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        // System.out.printf("=========================%n");
 
-        for (int i = 0; i < student1B.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i+1), student1B[i]);
-        }
-        System.out.printf("=========================%n");
+        // for (int i = 0; i < student1B.length; i++) {
+        //     System.out.printf("| %-3d | %-15s |%n", (i+1), student1B[i]);
+        // }
+        // System.out.printf("=========================%n");
 
         
-        System.out.printf("=========================%n");
+        // System.out.printf("=========================%n");
+        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        // System.out.printf("=========================%n");
+
+        // for (int i = 0; i < student1C.length; i++) {
+        //     System.out.printf("| %-3d | %-15s |%n", (i+1), student1C[i]);
+        // }
+        // System.out.printf("=========================%n");
+
+
+        // System.out.printf("=========================%n");
+        // System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        // System.out.printf("=========================%n");
+        // for (int i = 0; i < student1D.length; i++) {
+        //     System.out.printf("| %-3d | %-15s |%n", (i+1), student1D[i]);
+        // }
+        // System.out.printf("=========================%n");
+    }
+
+    public static void display(String[] student) {
+    System.out.printf("=========================%n");
         System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
         System.out.printf("=========================%n");
 
-        for (int i = 0; i < student1C.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i+1), student1C[i]);
-        }
-        System.out.printf("=========================%n");
-
-
-        System.out.printf("=========================%n");
-        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
-        System.out.printf("=========================%n");
-        
-        for (int i = 0; i < student1D.length; i++) {
-            System.out.printf("| %-3d | %-15s |%n", (i+1), student1D[i]);
+        for (int i = 0; i < student.length; i++) {
+            System.out.printf("| %-3d | %-15s |%n", (i+1), student[i]);
         }
         System.out.printf("=========================%n");
     }
---
+++ Praktikum13/Welcome223.java
@@ -0,0 +1,11 @@
+public class Welcome223 {
+    public static void main(String[] args) {
+            displayWelcomeMessage("Ani");
+            displayWelcomeMessage("Budi");
+            displayWelcomeMessage("Cica");
+            displayWelcomeMessage("Dodi");
+        }
+        public static void displayWelcomeMessage(String name) {
+            System.out.println("Selamat datang di Politekik Negeri Malang, " + name);
+        }
+    }
